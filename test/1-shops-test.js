82f4ecb4b09f0adb66f49e2377803ed6b010a085
Nicolás Passerini <npasserini@gmail.com>
2016-05-01 11:54:38 +0200

    Test GET /shops

--- test/1-shops-test.js
+++ test/1-shops-test.js
@@ -1,30 +1,34 @@
-var supertest = require("supertest");
-var should = require("should");
-require("should-http")
+import supertest from 'supertest'
+import 'should'
+import 'should-http'
 
 // This agent refers to PORT where program is runninng.
 
-var server = supertest.agent("http://localhost:9200");
+const server = supertest.agent("http://localhost:9200")
 
 // UNIT test begin
 
-describe("SAMPLE unit test",function(){
+describe("Resource /shops", () => {
 
   // #1 should return home page
 
-  it("should return home page",function(done){
+  it("GET should return a list of shops",function(done){
 
     // calling home page api
-    server.get("/shops").end(function(err,res){
+    server.get("/shops").end((err,res) => {
       res.status.should.equal(200)
       res.should.be.json()
       res.body.should.be.an.Object()
       res.body.should.have.keys('items', 'paging')
       res.body.items.should.be.an.Array()
+      res.body.items.forEach(item => {
+        item.should.be.an.Object()
+        item.should.have.keys('latitude', 'longitude', 'name', 'address', 'location', 'id')
+      })
 
       console.log(res.body)
-      done();
-    });
-  });
+      done()
+    })
+  })
 
-});
+})
